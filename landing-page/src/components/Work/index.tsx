f6efe8e3a0a40620fb14640778b69632d4e02373
izazalamin <izazalamin@gmail.com>
2024-09-30 10:01:11 +0700

    feat:adding some feature

--- landing-page/src/components/Work/index.tsx
+++ landing-page/src/components/Work/index.tsx
@@ -10,33 +10,33 @@ import Work7 from "../../assets/work-7.png";
 
 const Work: FC = () => {
   return (
-    <div className="container mx-auto shadow-md flex flex-col justify-center items-center">
-        <section className="max-w-7x1 overflow-x-scroll rounded-md flex flex-row justify-center items-center gap-10">
-            <div className="carousel-item bg-gray-300 shadow-md">
-                <img src={Work1} alt="work1" className="Work-1 p-10" />
+    <div className="w-full shadow-md flex flex-col justify-center items-center flex-nowrap relative overflow-visible">
+        <section className="overflow-x-scroll overflow-visible rounded-md flex-row justify-center items-center gap-10 transition-all hover:shadow-2xl flex scroll-smooth">
+            <div className="carousel-item bg-gray-300 shadow-md rounded-[4px]">
+                <img src={Work1} alt="work1" className="p-10" />
             </div>
-            <div className="carousel-item bg-gray-300 shadow-md">
-                <img src={Work2} alt="work1" className="Work-1 p-10" />
+            <div className="carousel-item bg-gray-300 shadow-md rounded-[4px]">
+                <img src={Work2} alt="work1" className="p-10" />
             </div>
-            <div className="carousel-item bg-gray-300 shadow-md">
-                <img src={Work3} alt="work1" className="Work-1 p-10" />
+            <div className="carousel-item bg-gray-300 shadow-md rounded-[4px]">
+                <img src={Work3} alt="work1" className="p-10" />
             </div>
-            <div className="carousel-item bg-gray-300 shadow-md">
-                <img src={Work4} alt="work1" className="Work-1 p-10" />
+            <div className="carousel-item bg-gray-300 shadow-md rounded-[4px]">
+                <img src={Work4} alt="work1" className="p-10" />
             </div>  
         </section>
-        <section className="overflow-x-scroll  rounded-md flex flex-row justify-center items-center gap-10 mt-10">
-        <div className="carousel-item bg-gray-300 shadow-md">
-                <img src={Work5} alt="work1" className="Work-1 p-10" />
+        <section className="overflow-x-scroll flex flex-row justify-center items-center gap-10 mt-10 hover:shadow-2xl overflow-visible w-full scroll-smooth ">
+        <div className="carousel-item bg-gray-300 shadow-md rounded-[4px]">
+                <img src={Work5} alt="work1" className="p-10" />
             </div>
-            <div className="carousel-item bg-gray-300 shadow-md">
-                <img src={Work6} alt="work1" className="Work-1 p-10" />
+            <div className="carousel-item bg-gray-300 shadow-md rounded-[4px]">
+                <img src={Work6} alt="work1" className="p-10" />
             </div>
-            <div className="carousel-item bg-gray-300 shadow-md">
-                <img src={Work7} alt="work1" className="Work-1 p-10" />
+            <div className="carousel-item bg-gray-300 shadow-md rounded-[4px]">
+                <img src={Work7} alt="work1" className="p-10" />
             </div>
-            <div className="carousel-item bg-gray-300 shadow-md">
-                <img src={Work1} alt="work1" className="Work-1 p-10" />
+            <div className="carousel-item bg-gray-300 shadow-md rounded-[4px]">
+                <img src={Work1} alt="work1" className="p-10" />
             </div> 
             
         </section>
