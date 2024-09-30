3871c1017320c6df7961a200e3a93a46dc4337d5
izazalamin <izazalamin@gmail.com>
2024-09-30 10:06:29 +0700

    feat:new rev

--- landing-page/src/components/Work/index.tsx
+++ landing-page/src/components/Work/index.tsx
@@ -10,7 +10,7 @@ import Work7 from "../../assets/work-7.png";
 
 const Work: FC = () => {
   return (
-    <div className="w-full shadow-md flex flex-col justify-center items-center flex-nowrap relative overflow-visible">
+    <div className="w-full shadow-md flex flex-col justify-center items-center flex-nowrap relative overflow-visible opacity-2">
         <section className="overflow-x-scroll overflow-visible rounded-md flex-row justify-center items-center gap-10 transition-all hover:shadow-2xl flex scroll-smooth">
             <div className="carousel-item bg-gray-300 shadow-md rounded-[4px]">
                 <img src={Work1} alt="work1" className="p-10" />
@@ -22,7 +22,7 @@ const Work: FC = () => {
                 <img src={Work3} alt="work1" className="p-10" />
             </div>
             <div className="carousel-item bg-gray-300 shadow-md rounded-[4px]">
-                <img src={Work4} alt="work1" className="p-10" />
+                <img src={Work1} alt="work1" className="p-10" />
             </div>  
         </section>
         <section className="overflow-x-scroll flex flex-row justify-center items-center gap-10 mt-10 hover:shadow-2xl overflow-visible w-full scroll-smooth ">
@@ -33,7 +33,7 @@ const Work: FC = () => {
                 <img src={Work6} alt="work1" className="p-10" />
             </div>
             <div className="carousel-item bg-gray-300 shadow-md rounded-[4px]">
-                <img src={Work7} alt="work1" className="p-10" />
+                <img src={Work2} alt="work1" className="p-10" />
             </div>
             <div className="carousel-item bg-gray-300 shadow-md rounded-[4px]">
                 <img src={Work1} alt="work1" className="p-10" />
